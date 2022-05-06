6d79ca82885340c8ea8c45be2715e42c14998b8b
monil-patel <45443099+monil-patel@users.noreply.github.com>
2022-05-06 21:02:01 +0000

    init playground repo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import React from "react";
+import { ThemeProvider } from "@fluentui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeProvider>
+        <h1>Testing 2</h1>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
